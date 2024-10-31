b4c5f56e28f3c009189442d27184987b7892cc68
Vaidas <vaidas.vaiciunas@inbox.lt>
2024-10-31 18:27:21 +0200

    Adds shadcn component support

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <>AWS IAM Engine</>;
+  return (
+    <div className="m-10 flex justify-center">
+      <Button>It works!</Button>
+    </div>
+  );
 }
 
 export default App;
